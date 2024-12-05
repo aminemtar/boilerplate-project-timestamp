95ae88fd6c47b17ecfb7a22344c44161fe681629
Mohamed Amine Mtar <mohamedamine.mtar@esprit.tn>
2024-12-05 03:30:51 +0100

    Completed timestamp microservice

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
 const express = require('express');
-const cors = require('cors'); // Import CORS middleware
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Use CORS
-app.use(cors());
+const PORT =  3000;
 
-// API endpoint
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 app.get('/api/:date?', (req, res) => {
   const dateParam = req.params.date;
 
   let date;
   if (!dateParam) {
-    // No date provided; use the current date
     date = new Date();
   } else if (!isNaN(dateParam)) {
-    // If the date is a number (timestamp)
     date = new Date(parseInt(dateParam));
   } else {
-    // If the date is a string
     date = new Date(dateParam);
   }
 
-  // Check if the date is valid
   if (isNaN(date.getTime())) {
     return res.json({ error: 'Invalid Date' });
   }
@@ -34,12 +29,6 @@ app.get('/api/:date?', (req, res) => {
   });
 });
 
-// Handle other routes
 app.use((req, res) => {
   res.status(404).json({ error: 'Not Found' });
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
